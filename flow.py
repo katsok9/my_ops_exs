8b1b07da2d378f6b6093fd65e84b0f89e685787e
katsok9 <katsok9@gmail.com>
2018-10-10 00:57:38 +0300

    change to f-strings

--- flow.py
+++ flow.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 
-import time
+
 import requests
 import tarfile
 import os
 import subprocess
 import sys
-from pprint import pprint
+
 
 
 # get images file
 url = 'https://s3.eu-central-1.amazonaws.com/devops-exercise/pandapics.tar.gz'
+repo_url = 'https://github.com/bigpandaio/ops-exercise'
 health_url = 'http://localhost:3000/health'
 filename = url[url.rfind("/")+1:]
-reponame = "ops-exercise"
+reponame = repo_url[repo_url.rfind("/")+1:]
 dirname = f"/{reponame}/public/images"
 cwd = os.getcwd()
 dir_path = f"{cwd}{dirname}"
@@ -22,19 +23,20 @@ repo_path = f"{cwd}/{reponame}"
 #clone git repo
 
 if not os.path.exists(repo_path):
-    ret_code = subprocess.call("git clone 'https://github.com/bigpandaio/ops-exercise' ", shell=True)
+    ret_code = subprocess.call(f"git clone '{repo_url}' ", shell=True)
     if not ret_code == 0:
-        print("Error cloning repo, check git config, or url: {}".format('https://github.com/bigpandaio/ops-exercise'))
+        print(f"Error cloning repo, check git config, or url: {repo_url}")
         sys.exit(1)
+else:
+    print(f"repo: {reponame} already cloned!")
 
-
-print("file name found: {}\nDownloading".format(filename))
+print(f"file name found: {filename}\nDownloading")
 
 r = requests.get(url)
 with open("pandapics.tar.gz", "wb") as code:
          code.write(r.content)
 code.close()
-print("Extracting {} to {}".format(filename,dir_path))
+print(f"Extracting {filename} to {dir_path}")
 # extract images into /public/images
 if not os.path.exists(dir_path):
     os.makedirs(dir_path)
@@ -52,18 +54,6 @@ if not ret_code == 0:
 
 cmd = "docker-compose up -d"
 (cmd_status, cmd_output) = subprocess.getstatusoutput(cmd)
-print("status: {}\noutput: '{}'".format(cmd_status, cmd_output))
+print(f"status: {cmd_status}\noutput: '{cmd_output}'")
 if not cmd_status == 0:
     sys.exit(1)
-
-# time.sleep(5)
-#
-# print("Checking app health")
-#
-# health_res = requests.get(health_url)
-#
-# print(health_res.status_code)
-#
-# if not health_res.status_code == 200:
-#     pprint(health_res.json())
-#     sys.exit(1)
